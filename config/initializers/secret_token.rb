052db81ca47f12fdaa7962c4482e8dd91a86f152
lonewolfgames <nathanfaucett@gmail.com>
2013-09-22 08:46:27 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BargainBox::Application.config.secret_key_base = '42ba007d3dd386701e8b283cac557390375450ad9062ef49042282f63e26126e5d358d476cc107fca0c559fe6e7679df26a4e857212061f4db0a55ab34d540fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BargainBox::Application.config.session_store :cookie_store, key: '_bargain_box_session'
