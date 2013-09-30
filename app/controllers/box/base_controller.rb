fee837aa6566375f61f0a7de9524a2cf44dffeac
John Faucett <jwaterfaucett@gmail.com>
2013-09-30 21:17:31 +0200

    Add simple auth via X-AUTH-TOKEN

--- app/controllers/box/base_controller.rb
+++ app/controllers/box/base_controller.rb
@@ -1,5 +1,14 @@
 class Box::BaseController < ApplicationController
   before_filter :authenticate_box_user!
+  prepend_before_filter :check_for_auth_token
   layout "box/app"
+
+   private
+    def check_for_auth_token
+      token = request.headers["X-AUTH-TOKEN"]
+      return unless token
+      user = User.where(auth_token: token).first
+      sign_in user if user
+    end
 
 end
