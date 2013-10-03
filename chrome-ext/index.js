9743874b5e7877194c746a432f2bac8e89fe7941
lonewolfgames <nathanfaucett@gmail.com>
2013-10-03 12:56:52 -0500

    updated chrome-ext

--- chrome-ext/index.js
+++
@@ -1,9 +0,0 @@
-(function( window, document, undefined ){
-	
-	window.onload = function(){
-		iframe = document.getElementById("iframe");
-		
-		iframe.contentWindow.document.body;
-	};
-	
-}( window, document, undefined ));
