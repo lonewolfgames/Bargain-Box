a0ac014c493b685610c053d1230a6b7067d1c127
John Faucett <jwaterfaucett@gmail.com>
2013-10-03 19:26:46 +0200

    Add Sidekiq

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,27 @@
 class Item < ActiveRecord::Base
+  # inheritance
+  include PageParser
 
   # relations
   belongs_to :cart
 
   # validations
   validates :title, :host, :url, presence: true
+
+  # states
+  state_machine initial: :created do
+
+    before_transition on: :parse, do: :parse_page
+    
+    event :parse do
+      transition created: :parsing
+    end
+
+    event :complete do
+      transition parsing: :completed
+    end
+
+  end
 
   def base_price
     read_attribute(:base_price).to_f / 100
