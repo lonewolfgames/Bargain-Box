abda4f9fbe5e1a42439642c0b4873146bb7b7a5a
John Faucett <jwaterfaucett@gmail.com>
2013-10-03 18:26:16 +0200

    Change Sidebar to Cart names

--- app/controllers/box/base_controller.rb
+++ app/controllers/box/base_controller.rb
@@ -1,15 +1,22 @@
 class Box::BaseController < ApplicationController
-  before_filter :authenticate_user!
-  prepend_before_filter :check_for_auth_token
+  before_action :authenticate_user!
+  before_action :find_base_data
+
+  prepend_before_action :check_for_auth_token
   layout "box/app"
   scoped_views = false
 
    private
+
     def check_for_auth_token
       token = request.headers["X-AUTH-TOKEN"]
       return unless token
       user = User.where(auth_token: token).first
       sign_in user if user
+    end
+
+    def find_base_data
+      @carts = current_user.carts.all
     end
 
 end
--- app/controllers/box/carts_controller.rb
+++ app/controllers/box/carts_controller.rb
@@ -1,28 +1,35 @@
 class Box::CartsController < Box::BaseController
   respond_to :html, :json
 
+  # GET /carts
   def index
     @carts = current_user.carts.all
     respond_with(@carts)
   end
   
+  # GET /carts/:id
   def show
     @cart = current_user.carts.find( params[:id] )
     respond_with(@cart)
   end
   
+  # GET /carts/new
   def new
     @cart = current_user.carts.new
     @cart.items.build
     respond_with(@cart)
   end
   
+  # POST /carts
   def create
     @cart = current_user.carts.new( cart_params )
     if @cart.save
       redirect_to carts_path
     else
-      render action: :new
+      respond_to do |wants|
+        wants.html  { render action: :new }
+        wants.js    { render json: @cart }
+      end
     end
   end
   
