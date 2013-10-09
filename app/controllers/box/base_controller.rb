07dcbd0fca59ef0770a39dbf0cbeb55cfa2d1780
lonewolfgames <nathanfaucett@gmail.com>
2013-10-08 19:18:30 -0500

    added sign in and save button to chrome ext

--- app/controllers/box/base_controller.rb
+++ app/controllers/box/base_controller.rb
@@ -1,19 +1,20 @@
 class Box::BaseController < ApplicationController
   before_action :authenticate_user!, :except => [:create, :new]
   before_action :find_base_data
+  skip_before_filter  :verify_authenticity_token
 
-  #prepend_before_action :check_for_auth_token
+  #prepend_before_action :check_for_auth_token, :except => [:create, :new]
   layout "box/app"
   scoped_views = false
 
   private
 
-  #def check_for_auth_token
-  #  token = request.headers["X-AUTH-TOKEN"]
-  #  return unless token
-  #  user = User.where(auth_token: token).first
-  #  sign_in user if user
-  #end
+  def check_for_auth_token
+    token = request.headers["X-AUTH-TOKEN"]
+    return unless token
+    user = User.where(auth_token: token).first
+    sign_in user if user
+  end
 
   def find_base_data
     @carts = current_user.carts.load
