797bc4398a2553f8c6065e152c51c4a2f8d815d6
John Faucett <jwaterfaucett@gmail.com>
2013-10-26 19:10:58 +0200

    Fix root scopes for normal site

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 BargainBox::Application.routes.draw do
   
   constraints(subdomain: /app(dev)?/) do
-    
     scope module: :box do
       devise_for :users, controllers: { sessions: "box/sessions", registrations: 'box/registrations' }
       resources :carts do
@@ -11,9 +10,9 @@ BargainBox::Application.routes.draw do
         root :to => "home#index", :as => "authenticated_root"
       end
     end
-    
-    root "home#index"
-    get "/about", to: "home#about"
-    get "/contact", to: "home#contact"
   end
+
+  root to: "home#index"
+  get "/about", to: "home#about"
+  get "/contact", to: "home#contact"
 end
