f9585baa8dc70b9d754469a6ba4f42798466ac88
John Faucett <jwaterfaucett@gmail.com>
2013-10-26 19:45:54 +0200

    Add Item Parsing Log info

--- app/workers/scraper_worker.rb
+++ app/workers/scraper_worker.rb
@@ -1,11 +1,14 @@
 class ScraperWorker
   include Sidekiq::Worker
-  sidekiq_options :retry => false
+  sidekiq_options :retry => 6
 
   def perform(item_id)
+
     item = Item.find(item_id)
+    logger.info { "Item: #{item.id}" }
     pp = PageParser.new(item)
     pp.parse!
+    logger.info { "Parsed!" }
   end
 
 end
